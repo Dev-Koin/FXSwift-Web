b7d95fc8564fabc83c23e6f7bea7cdfc3f30512a
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-04 14:49:13 +1200

    adding converter and modified app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,1 +1,13 @@
 import React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import converter from './converter'
+
+const App = props => {
+    return (
+        <Router>
+            <div>
+                
+            </div>
+        </Router>
+    )
+}
