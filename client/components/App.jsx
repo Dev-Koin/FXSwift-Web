927789d316ccb976749aff20266c1b5b9572c523
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-04 17:58:58 +1200

    added some functionality

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import converter from './converter'
+import {getCoversionUrl} from '../api.js'
 
 const App = props => {
     return (
--- client/components/converter.jsx
+++ client/components/converter.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export default class converter extends React.Component {
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+    render() {
+
+    }
 }
