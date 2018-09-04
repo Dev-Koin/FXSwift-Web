8b501e6daefa4162bb41424b2e1c479cb2db7f95
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-04 18:20:14 +1200

    modified converter jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import converter from './converter'
-import {getCoversionUrl} from '../api.js'
+// import {getCoversionUrl} from '../api.js'
 
 const App = props => {
     return (
         <Router>
             <div>
-                
+             <Route exact path="/" component={converter} />  
             </div>
         </Router>
     )
 }
+
+export default App
--- client/components/converter.jsx
+++ client/components/converter.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import {getCoversionUrl} from '../api.js'
 
 export default class converter extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            options=''
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange(e) {
+        var key = e.target.name
+        var value = e.target.value
+        this.setState({[key]: value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
     }
 
     render() {
