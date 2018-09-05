929710cbd51313d76b58c5f2a6ee12a9eeeb6492
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-05 16:04:44 +1200

    updated jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,14 +7,29 @@ export default class App extends React.Component {
     constructor(props) {
         super(props)
         this.state={
-            rates1: '',
-            rates2: '',
-            numbers:''
+            rates:[],
         }
-        this.state.rates1 = this.state.rates1.bind(this)
-        this.state.rates2 = this.state.rates2.bind(this)
+        this.updateRates = this.updateRates.bind(this)
+        // this.state.rates1 = this.state.rates1.bind(this)
+        // this.state.rates2 = this.state.rates2.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentDidMount(){
+        getConversion(this.updateRates) 
+    }
+
+    updateRates(err, res) {
+        let newRates = {
+            NZD: res.rates.NZD,
+            AUD: res.rates.AUD,
+            GBP: res.rates.GBP
+        }
+        this.setState({
+            rates: newRates
+        })
+        console.log('hey', newRates)
     }
 
     handleChange(e) {
@@ -26,20 +41,30 @@ export default class App extends React.Component {
     handleSubmit(e) {
         e.preventDefault()
     }
-
-    getConversion() {
-        this.state.rates1 = this.state.rates2
-        console.log('hai',rates1)
-    }
-
+// Try to use .map to render currencies
+// make sure storing from the API. make sure that what I wanted in state look like what it looks like
     render() {
+        console.log('hey', this.state)
         return (
+       <div>    
+                 
         <div class="form-group">
-            <label for="sel1">Select list (select one):</label>
+            <label for="sel1">Select Currency:</label>
                 <select class="form-control" id="sel1">
-                    <option>{this.state.rates1}</option>
+                    <option>{this.updateRates}</option>
+                    <option>{this.updateRates}</option>
+                    <option>{this.updateRates}</option>
                 </select>
         </div>
+        <div class="form-group">
+        <label for="sel1">Select Currency:</label>
+            <select class="form-control" id="sel1">
+                <option>{this.updateRates}</option>
+                <option>{this.updateRates}</option>
+                <option>{this.updateRates}</option>
+            </select>
+    </div>
+</div>
         )
     }
 }
