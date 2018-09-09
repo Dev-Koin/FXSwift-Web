459a9edb361c12529c047c6b217e77e2ea4d7d79
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-09 13:56:49 +1200

    modified the components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,70 +1,13 @@
 import React from 'react'
-// import { HashRouter as Router, Route, Link } from 'react-router-dom'
-import Idr from './Idr'
-import {getConversion} from '../api.js'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import Conversion from './Conversion'
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state={
-            rates: []
-        }
-        this.updateRates = this.updateRates.bind(this)
-        // this.state.rates1 = this.state.rates1.bind(this)
-        // this.state.rates2 = this.state.rates2.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    componentDidMount(){
-        getConversion(this.updateRates) 
-    }
-
-    updateRates(err, res) {
-        let newRates = {
-            NZD: res.rates.NZD,
-            IDR: res.rates.IDR,
-            RUB: res.rates.RUB
-        }
-        this.setState({
-            rates: newRates
-        })
-        console.log('hai', newRates)
-    }
-
-    handleChange(e) {
-        var key = e.target.name
-        var value = e.target.value
-        this.setState({[key]: value})
-    }
-
-    handleSubmit(e) {
-        e.preventDefault()
-    }
-// Try to use .map to render currencies
-// make sure storing from the API. make sure that what I wanted in state look like what it looks like
-    render() {
-        console.log('hey', this.state)
-        return (
-       <div>    
-                 
-        <div class="form-group">
-            <label for="sel1">Select Currency:</label>
-                <select class="form-control" id="sel1">
-                    <option>{this.state.rates.length !== 0 && this.state.newRates}NZD</option>
-                    <option>{this.state.rates.length !== 0 && this.state.newRates}IDR</option>
-                    <option>{this.state.rates.length !== 0 && this.state.newRates}RUB</option>
-                </select>
+const App = props => {
+    return (<Router>
+        <div>
+        <Route exact path="/" component={Conversion} />
         </div>
-        <div class="form-group">
-        <label for="sel1">Select Currency:</label>
-            <select class="form-control" id="sel1">
-                <option>{this.state.rates.length !== 0 && this.state.newRates}NZD</option>
-                <option>{this.state.rates.length !== 0 && this.state.newRates}IDR</option>
-                <option>{this.state.rates.length !== 0 && this.state.newRates}RUB</option>
-            </select>
-    </div>
-</div>
-        )
-    }
-} 
+    </Router>)
+}
+
+export default App
---
+++ client/components/Conversion.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {getConversion} from '../api.js'
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            rates: {}
+        }
+        this.updateRates = this.updateRates.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    componentDidMount(){
+        getConversion(this.updateRates)
+    }
+
+    updateRates(err, res) {
+        let newRates = {
+            NZD: res.rates.NZD,
+            IDR: res.rates.IDR,
+            RUB: res.rates.RUB
+        }
+        this.setState({
+            rates: newRates
+        })
+        console.log('hai', newRates)
+    }
+
+    handleChange(e) {
+        var key = e.target.name
+        var value = e.target.value
+        this.setState({[key]: value})
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+    }
+// Try to use .map to render currencies
+// make sure storing from the API. make sure that what I wanted in state look like what it looks like
+    render() {
+        console.log('hey', this.state)
+        return (
+       <div>    
+                 
+        <div class="form-group">
+            <label for="sel1">Select Currency:</label>
+                <select class="form-control" id="sel1">
+                    <option>{this.state.rates.length !== 0 && this.state.newRates}NZD</option>
+                    <option>{this.state.rates.length !== 0 && this.state.newRates}IDR</option>
+                    <option>{this.state.rates.length !== 0 && this.state.newRates}RUB</option>
+                </select>
+                <input type="text"></input>
+        </div>
+        <div class="form-group">
+        <label for="sel1">Select Currency:</label>
+            <select class="form-control" id="sel1">
+                <option>{this.state.rates.length !== 0 && this.state.newRates}NZD</option>
+                <option>{this.state.rates.length !== 0 && this.state.newRates}IDR</option>
+                <option>{this.state.rates.length !== 0 && this.state.newRates}RUB</option>
+            </select>
+            <input type="text"></input>
+    </div>
+</div>
+        )
+    }
+} 
