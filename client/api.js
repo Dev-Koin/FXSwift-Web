92a3bb4f8196f0b959873f451c6fd5de73c05e60
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-09-16 11:40:27 +1200

    change to old API

--- client/api.js
+++ client/api.js
@@ -1,6 +1,6 @@
 import request from 'superagent'
 
-const getConversionUrl = 'http://data.fixer.io/api/latest?access_key=59fbf81af567c668fc4694fb20a783ba&format=1'
+const getConversionUrl = 'https://api.exchangeratesapi.io/latest'
 
 export function getConversion (callback) {
   request
