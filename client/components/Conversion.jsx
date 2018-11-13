2d5f93bc697ba58a22da5b9c40e885ed460df75c
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-11-14 12:17:28 +1300

    try another method

--- client/components/Conversion.jsx
+++ client/components/Conversion.jsx
@@ -1,28 +1,22 @@
 import React from 'react'
-import Rupiah from './Rupiah'
+//import Price from 'react-forex-price'
 import {getConversion} from '../api.js'
 
 export default class Conversion extends React.Component {
     constructor(props) {
         super(props)
         this.state={
-            rates: []
+            rates: getConversion
         }
         this.updateRates = this.updateRates.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+        // this.handleChange = this.handleChange.bind(this)
+        // this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount(){
         getConversion(this.updateRates)
     }
 
-    // updateRates(err, res) {
-    //     this.setState(
-    //         {rates: res.rates}
-    //     )
-    //     console.log('halo', this.rates)
-    // }
 
     updateRates(err, res) {
         let newRates = {
@@ -31,46 +25,28 @@ export default class Conversion extends React.Component {
             RUB: res.rates.RUB
         }
         this.setState({
-            rates: newRates
+            rates: this.newRates
         })
         console.log('halo', newRates)
     }
 
-    handleChange(e) {
-        var key = e.target.name
-        var value = e.target.value
-        this.setState({[key]: value})
-    }
+//    handleChange(e) {
+//        var key = e.target.name
+//        var value = e.target.value
+//        this.setState({[key]: value})
+//    }
 
-    handleSubmit(e) {
-        e.preventDefault()
-    }
+    // handleSubmit(e) {
+    //     e.preventDefault()
+    // }
 // Try to use .map to render currencies
 // make sure storing from the API. make sure that what I wanted in state look like what it looks like
     render() {
         console.log('hey', this.state)
         return (
-            <div>    
-                 
-            <div class="form-group">
-                <label for="sel1">Select Currency:</label>
-                    <select class="form-control" id="sel1">
-                        <option>{this.state.rates.length !== 0 && <Rupiah rates={this.state.rates.IDR}/>}IDR</option>
-                        <option>{this.state.rates.length !== 0 && <Rupiah rates={this.state.rates.IDR}/>}RUB</option>
-                        <option>{this.state.rates.length !== 0 && <Rupiah rates={this.state.rates.IDR}/>}NZD</option>
-                    </select>
-                    <input type="text"></input>
+            <div>
+                <p>Today's rubel is {this.state}</p>
             </div>
-            <div class="form-group">
-            <label for="sel1">Select Currency:</label>
-                <select class="form-control" id="sel1">
-                <option>{this.state.rates.length !== 0 && <Rupiah rates={this.state.rates.IDR}/>}IDR</option>
-                <option>{this.state.rates.length !== 0 && <Rupiah rates={this.state.rates.IDR}/>}RUB</option>
-                <option>{this.state.rates.length !== 0 && <Rupiah rates={this.state.rates.IDR}/>}NZD</option>
-                </select>
-                <input type="text"></input>
-        </div>
-    </div>
         )
     }
 } 
