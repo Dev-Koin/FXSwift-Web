8e2d625189ceae11c0076d6f943c23076923b9ca
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2018-12-12 15:38:06 +1300

    modified a little bit

--- client/components/Conversion.jsx
+++ client/components/Conversion.jsx
@@ -30,24 +30,25 @@ export default class Conversion extends React.Component {
     console.log('halo', newRates)
 }
 
-//    handleChange(e) {
-//        var key = e.target.name
-//        var value = e.target.value
-//        this.setState({[key]: value})
-//    }
+   handleChange(e) {
+       var key = e.target.name
+       var value = e.target.value
+       this.setState({[key]: value})
+   }
 
-    // handleSubmit(e) {
-    //     e.preventDefault()
-    // }
+    handleSubmit(e) {
+        e.preventDefault()
+    }
 // Try to use .map to render currencies
 // make sure storing from the API. make sure that what I wanted in state look like what it looks like
     render() {
         console.log('hey', this.state)
         return (
             <div>
-                <p>Today's Russian Rubel is {this.state.rates.RUB}</p>
-                <p>Today's Indonesian Rupiah is {this.state.rates.IDR}</p>
-                <p>Today's New Zealand Dollar is {this.state.rates.NZD}</p>
+                <p>Today's Russian Rubel is {this.state.rates.RUB} and it is equal to 1 USD</p>
+                <input onChange={this.handleChange}></input>
+                <p>Today's Indonesian Rupiah is {this.state.rates.IDR} and it is equal to 1 USD</p>
+                <p>Today's New Zealand Dollar is {this.state.rates.NZD} and it is equal to 1 USD</p>
             </div>
         )
     }
